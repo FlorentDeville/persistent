118b700d6142201275873be6d6375a82de1e1e25
Florent <florent.devillechabrol@gmail.com>
2014-12-08 18:53:07 +0100

    Add statistics to pawns.

---
+++ Assets/Scripts/Entities/Combat/PawnStatistics.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PawnStatistics : MonoBehaviour 
+{
+
+    public float m_Atk;
+
+    public float m_Def;
+
+    public float m_MaxHP;
+
+    public float m_HP;
+
+    public float m_MaxMP;
+
+    public float m_MP;
+
+    public float m_MGAtk;
+
+    public float m_MGDef;
+
+    public float m_WepAtk { get; set; }
+
+    public float m_WepAtkCritic { get; set; }
+
+    public float m_WepDef { get; set; }
+
+    public float m_WepDefCritic { get; set; }
+
+    public float m_WepMGAtk { get; set; }
+
+    public float m_WepMGAtkCritic { get; set; }
+
+    public float m_WepMGDef { get; set; }
+
+    public float m_WepMGDefCritic { get; set; }
+
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
