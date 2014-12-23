44a87edd0ecc074cd433d988f18f184aa96ba002
Florent <florent.devillechabrol@gmail.com>
2014-12-23 17:22:01 +0100

    Add ending cutscenes
    - Add new trigger to interact with colliders
    - Add new interacation to load new scene.
    - Add new cutscene.

---
+++ Assets/Scripts/Interactions/InteractionLoadScene.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Interactions
+{
+    public class InteractionLoadScene : Interaction 
+    {
+        public string m_SceneName;
+
+        public override void ExecuteOnEnable()
+        {
+            UnityEngine.Application.LoadLevel(m_SceneName);
+        }
+    }
+}
---
+++ Assets/Scripts/Interactions/Trigger/Trigger_EnterCollider.cs
@@ -0,0 +1,32 @@
+﻿using AssemblyCSharp;
+using UnityEngine;
+
+namespace Assets.Scripts.Interactions.Trigger
+{
+    public class Trigger_EnterCollider : MonoBehaviour
+    {
+        public Collider m_collider;
+
+        public Interaction[] m_interactions;
+
+        public bool m_AutoReset;
+
+        void Awake()
+        {
+            foreach (Interaction inter in m_interactions)
+                inter.enabled = false;
+        }
+
+        void OnTriggerEnter(Collider _col)
+        {
+            if (_col.gameObject.name != GameObjectHelper.getPlayerName())
+                return;
+
+            foreach (Interaction inter in m_interactions)
+                inter.enabled = true;
+
+            if (!m_AutoReset)
+                enabled = false;
+        }
+    }
+}
