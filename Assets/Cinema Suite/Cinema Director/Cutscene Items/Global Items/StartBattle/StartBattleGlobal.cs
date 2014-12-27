abc04361cc348243d8a5b7c58ecc910a8e72e9b6
Florent <florent.devillechabrol@gmail.com>
2014-12-27 14:49:26 +0100

    Add intro sequence of side boss

---
+++ Assets/Cinema Suite/Cinema Director/Cutscene Items/Global Items/StartBattle/StartBattleGlobal.cs
@@ -0,0 +1,37 @@
+﻿using Assets.Scripts.Entities.World;
+using Persistent.WorldEntity;
+
+namespace CinemaDirector
+{
+    [CutsceneItemAttribute("Battle", "Start Battle")]
+    public class StartBattleGlobal : CinemaGlobalEvent
+    {
+        public BaseEnemy_CombatSettings m_Settings;
+
+        //public override void Initialize()
+        //{
+        //    if (target != null)
+        //    {
+        //        cachedState = target.activeInHierarchy;
+        //    }
+        //}
+
+        public override void Trigger()
+        {
+            LevelMaster.GetInstance().StartBattleTransition(m_Settings);
+        }
+
+        //public override void Reverse()
+        //{
+        //    if (target != null)
+        //    {
+        //        target.SetActive(previousState);
+        //    }
+        //}
+
+        //public override void Stop()
+        //{
+        //    target.SetActive(cachedState);
+        //}
+    }
+}
