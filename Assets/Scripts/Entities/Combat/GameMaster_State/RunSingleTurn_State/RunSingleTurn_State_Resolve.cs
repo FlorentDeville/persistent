2990ea9eeffe3da530cab48a8a532ef538ad40cd
Florent <florent.devillechabrol@gmail.com>
2015-01-11 16:45:52 +0100

    Battle
    Fix resolve not being executed.

--- Assets/Scripts/Entities/Combat/GameMaster_State/RunSingleTurn_State/RunSingleTurn_State_Resolve.cs
+++ Assets/Scripts/Entities/Combat/GameMaster_State/RunSingleTurn_State/RunSingleTurn_State_Resolve.cs
@@ -21,12 +21,13 @@ public partial class GameMaster : MonoBehaviour
                 //compute damage
                 ActionRunner act = m_Behavior.GetRunSingleTurnState().GetSelectedAction();
 
+                PawnStatistics src = act.m_Pawn.GetComponent<PawnStatistics>();
+                PawnStatistics target = act.m_Target.GetComponent<PawnStatistics>();
+                ResolveResult result = new ResolveResult();
+                act.ActionDescription.m_Power.Resolve(src, target, result);
+
                 if (act.ActionDescription.m_Power.m_DamageTarget == PawnTag.HP)
-                {
-                    PawnStatistics src = act.m_Pawn.GetComponent<PawnStatistics>();
-                    PawnStatistics target = act.m_Target.GetComponent<PawnStatistics>();
-                    ResolveResult result = new ResolveResult();
-                    act.ActionDescription.m_Power.Resolve(src, target, result);
+                {    
                     Vector3 effectPosition = Camera.main.WorldToScreenPoint(act.m_Target.transform.position);
                     m_Behavior.m_UIEffects.StartDamageEffect(effectPosition, result.m_Damage.ToString());
                 }
